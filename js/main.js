02f3d0f9194b9c2c9cc23642e8ca984676e20346
cosource <richard@cosource.com.au>
2015-04-02 18:53:20 +0200

    Animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 angular.module('madlibs', [])
+angular.module('madlibs', ['ngAnimate'])
 	.constant('GENDER', 'f')
+    .controller('mainController', function ($scope) {
+        $scope.showInput = true;
+        $scope.showOutput = false;
+       
+
+        
+
+        $scope.submit = function () {
+            $scope.showInput = $scope.formWords.$invalid;
+            $scope.showOutput = $scope.formWords.$valid;
+        };
+
+        $scope.startover = function () {
+            $scope.persons_name = '';
+            $scope.dirty_task = '';
+            $scope.obnoxious_celebrity = '';
+            $scope.job_title = '';
+            $scope.celebrity = '';
+            $scope.huge_number = '';
+            $scope.tedious_task = '';
+            $scope.useless_skill = '';
+            $scope.adjective = '';
+            $scope.formWords.$setPristine();
+            $scope.showInput = true;
+            $scope.showOutput = false;
+        };
+    })
 	.controller('genderController', function(GENDER, $scope){
 			$scope.gender = GENDER;
 			if ($scope.gender ==  'm'){
